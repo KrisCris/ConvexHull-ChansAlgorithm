361473f8ad6f86708670600787a4fcb324dcbe3a
Conn_Lost <hi.imhpc@outlook.com>
2021-12-13 01:36:38 -0600

    test

---
+++ src/views/ChanAlg.vue
@@ -0,0 +1,22 @@
+<template>
+    <Nav></Nav>
+    <h1>Chan's Alg</h1>
+</template>
+
+<script>
+import * as d3 from "d3";
+import Nav from "../components/Nav.vue";
+
+export default {
+    name: "ChanAlg",
+    components: { Nav },
+    data() {
+        return {};
+    },
+
+    mounted() {},
+};
+</script>
+
+<style>
+</style>
