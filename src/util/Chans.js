6cc5a74bb51c2fd12d370cf71eeb4f3a5ec2d460
Conn_Lost <hi.imhpc@outlook.com>
2021-12-14 02:03:32 -0600

    partially done, adapt for running in node

--- src/util/Chans.js
+++ src/util/Chans.js
@@ -1,10 +1,53 @@
-import Vertex from "./Vertex.js";
-// const Vertex = require('./Vertex').Vertex
+// import Vertex from "./Vertex.js";
+const Vertex = require('./Vertex')
 class Chans {
+    #P;
+    #subP = [];
+    #subCH = [];
 
-    static GrahamScan(vertices) {
-        let xSorted = vertices.sort(Vertex.xSort);
-        
+    constructor(P){
+        this.#P = P
+    }
+
+    Hull(P) {
+        for (let t = 0; true; t++) {
+            let m = Math.min(Math.pow(2,Math.pow(2,t)), P.length)
+            let L = this.PartialHull(P, m)
+            if(L != false){
+                return L
+            }
+        }
+    }
+
+    PartialHull(m) {
+        let r = Math.ceil(this.#P.length/m)
+
+        // Divide P into P1, P2, ... Pr
+        this.#subP = []
+        for(let i = 0; i < r; i++){
+            this.#subP.push(this.#P.slice(m*i, m*(i+1)))
+        }
+
+        // Compute CH for each Pi
+        for(let p of this.#subP){
+            this.#subCH.push(this.GrahamScan(p))
+        }
+
+        // JarvisMarch
+
+
+
+
+
+
+    }
+
+
+
+
+    GrahamScan(P) {
+        let xSorted = P.sort(Vertex.xSort);
+
         // Compute the lower hull
         let chLower = [xSorted[0], xSorted[1]]
         for (let i = 2; i < xSorted.length; i++) {
@@ -33,4 +76,13 @@ class Chans {
     JarvisMarch() {
 
     }
+}
+
+// your code here
+let P = [new Vertex(1,3), new Vertex(2,6), new Vertex(-1,4), new Vertex(-9, 2), new Vertex(-6, -2), new Vertex(5,-3)]
+let chans = new Chans(P)
+// let result = chans.Hull()
+let result = chans.GrahamScan(P)
+for(let p of result){
+    console.log(p.xPos, p.yPos)
 }
