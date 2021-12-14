2ae84d2badde220b20c4ec574e78956507c3903e
CocoYard <93499248+CocoYard@users.noreply.github.com>
2021-12-14 04:42:50 -0600

    Update Chans.js
    added a binary search function to return a vertex in a polygon such that it forms the largest angle with a given line segment

--- src/util/Chans.js
+++ src/util/Chans.js
@@ -72,12 +72,36 @@ class Chans {
 
         return chLower.concat(chUpper)
     }
-
+	
+	
     JarvisMarch() {
 
     }
 }
 
+function bSearch(P, i, j, a, b) {
+    if (i==j) 
+        return P[i];
+    if (Vertex.degree(a, b, P[i]) > Vertex.degree(a, b, P[j])) {
+        if (Vertex.degree(a, b, P[Math.floor((i+j)/2)]) < Vertex.degree(a, b, P[j]))
+            return bSearch(P, i, Math.floor((i+j)/2), a, b);
+        else if (Vertex.degree(a, b, P[Math.floor((i+j)/2 + 1)]) > Vertex.degree(a, b, P[Math.floor((i+j)/2)]))
+            return bSearch(P, Math.floor((i+j)/2) + 1, j, a, b);
+        else if (Vertex.degree(a, b, P[Math.floor((i+j)/2 + 1)]) == Vertex.degree(a, b, P[Math.floor((i+j)/2)]))
+            return P[Math.floor((i+j)/2)];
+        else return bSearch(P, i, Math.floor((i + j)/2), a, b);
+    }
+    else {
+        if (Vertex.degree(a, b, P[Math.floor((i+j)/2)]) < Vertex.degree(a, b, P[j]))
+            return bSearch(P, Math.floor((i+j)/2) + 1, j, a, b);
+        else if (Vertex.degree(a, b, P[Math.floor((i+j)/2 + 1)]) > Vertex.degree(a, b, P[Math.floor((i+j)/2)]))
+            return bSearch(P, Math.floor((i+j)/2) + 1, j, a, b);
+        else if (Vertex.degree(a, b, P[Math.floor((i+j)/2 + 1)]) == Vertex.degree(a, b, P[Math.floor((i+j)/2)]))
+            return P[Math.floor((i+j)/2)];
+        else return bSearch(P, i, Math.floor((i + j)/2), a, b);
+    }
+
+};
 // your code here
 let P = [new Vertex(1,3), new Vertex(2,6), new Vertex(-1,4), new Vertex(-9, 2), new Vertex(-6, -2), new Vertex(5,-3)]
 let chans = new Chans(P)
@@ -86,3 +110,7 @@ let result = chans.GrahamScan(P)
 for(let p of result){
     console.log(p.xPos, p.yPos)
 }
+let a = new Vertex(7, 0);
+let b = new Vertex(7, 1);
+let newRes = bSearch(result, 0, 3, a, b);
+console.log("The largest angle with line (7, 0) (7, 1) is made by:", newRes.xPos, newRes.yPos)
