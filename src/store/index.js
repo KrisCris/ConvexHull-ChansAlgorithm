6c37219b889fd6ec1a4504d0ec42fc24c5aedf3d
Conn_Lost <hi.imhpc@outlook.com>
2021-12-15 04:27:29 -0600

    init store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+    // data
+    // $store.state.stateName
+    // alternatively, computed -> getXXX() -> return this.$store.state.stateName
+    // computed -> setXXX(val) -> this.$store.commit("mutationName", arg)
+    state: {
+    },
+    // [commit] mutations
+    // $store.commit('mutationName', {})
+    mutations: {
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    // [dispatch] action
+    // $store.dispatch('actionName')
+    // can access state but can't change state.
+    // actions => commit mutation => change data
+    actions: {
+    },
+    // get data from state
+    // $store.getters.getterName
+    getters: {
+    },
+    // break state into multiple
+    modules: {
+    }
 })
