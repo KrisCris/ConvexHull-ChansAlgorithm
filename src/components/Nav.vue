ab30bedb705a3eee068561a2558ce377dd53960d
Conn_Lost <hi.imhpc@outlook.com>
2021-12-18 03:43:49 -0600

    add option to change animation speed

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,46 @@
     <div id="nav" class="top">
         <router-link :to="{ name: 'ChansAlg' }">Chan's Algorithm</router-link>
         <router-link :to="{ name: 'About' }">About </router-link>
+        <a class="settingsBtn" href="#" @click="settings=!settings"><font-awesome-icon icon="cog" /></a>
+        <div v-if="settings" class="settings" :style="{top:top()+'px', right:right()+'px'}">
+            <p>Animation Speed ({{speed}} ms)</p>
+            <input type="range" v-model="speed" min="10" max="100" step="10" >
+            <p style="font-size:0.7rem; color:gray">Only apply before animation.</p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "Nav",
-    methods: {},
+    methods: {
+        top(){
+            return document.querySelector('#nav').clientHeight+5
+        },
+        right(){
+            return window.innerWidth - document.querySelector('.settingsBtn').offsetLeft - document.querySelector('.settingsBtn').offsetWidth/2
+        }
+    },
+    data(){
+        return {
+            settings:false
+        }
+    },
+    // mounted(){
+    //     document.addEventListener('click',()=>{
+    //         this.settings = false
+    //     }, true)
+    // },
+    computed:{
+        speed: {
+            get() {
+                return this.$store.state.speed;
+            },
+            set(value) {
+                this.$store.commit("setSpeed", value);
+            },
+        },
+    }
 };
 </script>
 
@@ -27,6 +60,31 @@ export default {
     filter: brightness(120%);
     transition: all 0.3s ease-in-out;
     box-shadow: #2c2c2c 5px 5px 1rem;
+}
+
+.settings{
+    z-index:99;
+    position:fixed;
+    width: 14rem;
+    height:5rem;
+    background-color: rgb(48, 45, 45);
+    border-radius: 1rem;
+    padding-top: 1rem;
+    box-shadow: #2c2c2c 5px 5px 1rem;
+
+}
+.settings p{
+    color: white;
+    text-align: center;
+    margin: 0px;
+    padding: 0px;
+    margin-bottom: 0.5rem;
+}
+
+.settings input{
+    margin: 0px;
+    padding: 0px;
+    margin-bottom: 0.5rem;
 }
 
 @media only screen and (max-width: 992px) {
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 const app = createApp(App)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faCog)
 app.use(store)
 app.use(router)
 
